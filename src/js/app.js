c5799d03ad01024f0acf4b513226fd5dcb1d8d08
AlenaKarpolenko <alenac190590@mail.ru>
2022-11-07 19:23:29 +0700

    3

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import Chaos from './Chaos';
 
-const chaos = new Chaos(document.querySelector('.chaos_organizer'), 'https://https://ahj-diploma-server.vercel.app');
+const chaos = new Chaos(document.querySelector('.chaos_organizer'),'https://ahj-diploma-server-lwfl-3f3508xbu-alenakarpolenko.vercel.app');
 chaos.init();
